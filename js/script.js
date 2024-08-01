9a75dcd556fd0f8c302f929ce01558d435ed5208
Chill <78692505+Krisen@users.noreply.github.com>
2024-08-01 22:16:44 +0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let tg = window.Telegram.WebApp;
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+let feedback = document.getElementById("feedback");
+let safe = document.getElementById("safe");
+safe.addEventListener("click", () => {
+    tg.close
+});
